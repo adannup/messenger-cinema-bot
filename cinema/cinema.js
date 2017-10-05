60589cd70ce4ac5523955c53b49b4a04676c3c55
Ramon Adan <adannup@users.noreply.github.com>
2017-10-05 14:59:32 -0500

    Add some logic searching in cinema app

--- cinema/cinema.js
+++ cinema/cinema.js
@@ -1,55 +1,115 @@
 const request = require('request');
+const _ = require('lodash');
 
 var getCities = () => {
-  request({
-    url: 'http://www.cinepolis.com/manejadores/CiudadesComplejos.ashx',
-    qs: {
-      EsVIP: false
-    },
-    method: 'POST',
-    json: true
-  }, (error, response, body) => {
-    var data = body;
+  return new Promise((resolve, reject) => {
+    request({
+      url: 'http://www.cinepolis.com/manejadores/CiudadesComplejos.ashx',
+      qs: {
+        EsVIP: false
+      },
+      method: 'POST',
+      json: true
+    }, (error, response, body) => {
+       if(!error && response.statusCode === 200){
+        var data = body;
 
-    data.forEach((complejos, index) => {
-      console.log(`${index + 1}: ${complejos.Nombre}`);
+        var cities = data.map((complejos, index) => {
+          return (`${index + 1}: ${complejos.Nombre}`);
+        });
+        resolve(cities);
+      }else{
+        reject(`An error ocurr retrieving data from API cinepolis: ${response.statusCode}`);
+      }
     });
   });
 }
 
 var getCinemas = (city) => {
-  var encodedCity = city.trim().replace(/\s/g,"-");
-  request({
-    uri: 'http://www.cinepolis.com/Cartelera.aspx/GetNowPlayingByCity',
-    body: {
-      claveCiudad: encodedCity,
-      esVIP: false
-    },
-    method: 'POST',
-    json: true
-  }, (error, res, body) => {
-    var data = body.d.Locations;
-
-    Object.keys(data).forEach((location, index, array) => {
-      console.log(data[location]);
-    })
-
-  })
+  requestCinema(city)
+    .then((body) => {
+     var data = body.d.Locations;
+      var cinemas = Object.keys(data).map((location, index, array) => {
+        return data[location];
+      });
+    console.log(cinemas);
+  }).catch((messageError) => {
+    console.log(messageError);
+  });
 }
 
-// request({
-//   url: 'http://www.cinepolis.com/Cartelera.aspx/GetNowPlayingByCity',
-//   method: 'POST',
-//   body: {
-//     claveCiudad: estateEncoded,
-//     esVIP:false
-//   },
-//   json: true
-// }, (error, res, body) => {
-//
-// });
+var getMovies = (city) => {
+  requestCinema(city).
+    then((body) => {
+      var data = body.d.Cinemas;
+      var movies = [];
+
+      data.forEach((cinema) => {
+        // console.log(cinema);
+        var cinemaName = cinema.Name;
+        var cinemaKey = cinema.Key;
+        cinema.Dates.forEach((dates) => {
+          // console.log(dates);
+          var showtimeDate = dates.ShowtimeDate;
+          dates.Movies.forEach((movie) => {
+            // console.log(movie);
+            var movieTitle = movie.Title;
+            var movieKey = movie.Key;
+            movies.push(movieTitle);
+          });
+        })
+      })
+
+      var moviesFiltered = _.uniq(movies);
+      console.log(moviesFiltered);
+    })
+    .catch((messageError) => {
+      console.log(messageError);
+  });
+}
+
+var getMoviesFromCinema = (city, cinema) => {
+  var cinemaKey = 'cinepolis-plaza-el-dorado'
+  requestCinema(city)
+    .then((body) => {
+      var moviesData = body.d.Cinemas.filter((cinemas) => {
+        return cinemas.Key === cinemaKey;
+      });
+        console.log(moviesData[0]);
+    })
+    .catch((messageError) => {
+      console.log(messageError);
+    });
+}
+
+var requestCinema = (city) => {
+  return new Promise((resolve, reject) => {
+    var encodedCity = city.trim().replace(/\s/g,"-");
+
+    request({
+      uri:  'http://www.cinepolis.com/Cartelera.aspx/GetNowPlayingByCity',
+      body: {
+        claveCiudad: encodedCity,
+        esVIP: false
+      },
+      method: 'POST',
+      json: true
+    }, (error, response, body) => {
+        if(!error && response.statusCode === 200 && body.d !== null){
+          resolve(body);
+        }else if(body.d === null && response.statusCode === 200){
+          reject('Unable to find that city');
+        }else{
+          reject(`An error ocurr retrieving data from API cinepolis: ${response.statusCode}`);
+        }
+    });
+  });
+
+}
 
 module.exports = {
   getCities,
-  getCinemas
+  getCinemas,
+  getMovies,
+  getMoviesFromCinema
 }
--- cinema_app.js
+++ cinema_app.js
@@ -4,10 +4,18 @@ const cinema = require('./cinema/cinema');
 
 const argv = yargs
   .command('ciudades', 'Lista todas las ciudades que cuentan con complejos cinepolis')
+  .command('peliculas', 'Lista todas las peliculas', {
+    ciudad: {
+      alias: 'c',
+      demandOption: true,
+      describe: 'Ciudad o Estado',
+      type: 'string'
+    }
+  })
   .command('complejos', 'Muestra los complejos que existen en la ciudad', {
     ciudad: {
       alias: 'c',
-      demandOption: 'true',
+      demandOption: true,
       describe: 'Ciudad o Estado',
       type: 'string'
     }
@@ -26,7 +34,15 @@ const argv = yargs
 const command = argv._[0];
 
 if(command === 'ciudades') {
-  cinema.getCities();
+  cinema.getCities().then((message) => {
+    console.log(message);
+  }).catch((errorMessage) => {
+    console.log(errorMessage);
+  });
 }else if(command === 'complejos') {
   cinema.getCinemas(argv.ciudad);
+}else if(command === 'peliculas') {
+  cinema.getMovies(argv.ciudad);
+}else if(command === 'cine'){
+  cinema.getMoviesFromCinema(argv.ciudad, argv.complejo)
 }
